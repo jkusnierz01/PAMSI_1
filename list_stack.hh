ab1324f54a71f26ad37ccfb5bc48cd4448784799
jkusnierz01 <jedrek33@onet.pl>
2023-03-12 16:15:53 +0100

    list_stack_final

---
+++ list_stack.hh
@@ -0,0 +1,86 @@
+#ifndef LIST_STACK_HH
+#define LIST_STACK_HH
+
+#include <iostream>
+using namespace std;
+/*
+        KLASA: Element naszej listy jednokierunkowej - dowolnego typu - takiego samego jak stos, ktory bedziemy deklarowac
+Parametry:
+    - pointer na poprzedni element listy - przy konstruowaniu obiektu na NULL
+    - wartość ktora deklaruje uzytkownik
+*/
+template<typename Type>
+class list_element
+{
+public:
+    list_element *pointer;
+    Type value;
+    list_element(Type v)
+    {
+        pointer = NULL;
+        value = v;
+    };
+};
+
+/*
+KLASA: Stos opartu na liście jednokierunkowej
+Parametry:
+    - wskaznik na element list o zadeklarowanym typie
+Metody:
+    - push
+    - pop
+    - show
+    - remove all
+*/
+template<typename Type2>
+class list_stack
+{
+private:
+    list_element<Type2>* TopPointer;
+public:
+    list_stack()                    //konstruktor - utworzenie wskaznika na obiekt typu element_listy
+    {
+        TopPointer = NULL;
+    };
+    void push(Type2 number);        // dodanie elementu na stos
+    void pop();                     // usuniecie elementu ze stosu
+    void show();                    //wyswietlenie ostatniego elementu stosu
+    void remove_all();              //usuniecie wszystkich elementow stosu
+
+};
+
+template <typename Type2>
+void list_stack<Type2>::push(Type2 number)
+{
+    list_element<Type2>* tmp = this->TopPointer;        //wskaznik pomocniczy - aby nowy element stosu wskazywał na poprzedni
+    this->TopPointer = new list_element<Type2>(number);
+    this->TopPointer->pointer = tmp;
+}
+
+template <typename Type2>
+void list_stack<Type2>::pop()
+{
+    list_element<Type2>* tmp = this->TopPointer; 
+    this->TopPointer = this->TopPointer->pointer;
+    delete tmp;
+}
+
+template <typename Type2>
+void list_stack<Type2>::show()
+{   
+    if (this->TopPointer != NULL)
+{
+    cout << "Ostatni element stosu to: " << this->TopPointer->value << "\n";
+}
+}
+
+template <typename Type2>
+void list_stack<Type2>::remove_all()
+{
+while (this->TopPointer != NULL)
+{
+    this->pop();
+}
+}
+
+#endif
--- mainn.cpp
+++ mainn.cpp
@@ -1,21 +1,23 @@
 #include "array_stack.hh"
+#include "list_stack.hh"
 
 
 int main() 
 {
-    int liczba = 0;
-    cout << "Podaj rozmiar tablicy: " << "\n";
-    cin >> liczba;
-    array_stack <int> ob1(liczba);
-    ob1.push(33);
-    ob1.push(11);
-    ob1.show();
-    ob1.pop();
-    ob1.show();
-    ob1.push(2);
-    ob1.push(3);
-    ob1.remove_all();
-    ob1.show();
+    // int liczba = 0;
+    // cout << "Podaj rozmiar tablicy: " << "\n";
+    // cin >> liczba;
+    // array_stack <int> ob1(liczba);
+    list_stack<int> ob2;
+    ob2.push(22);
+    ob2.push(55);
+    ob2.show();
+    ob2.pop();
+    ob2.show();
+    ob2.push(222);
+    ob2.push(535);
+    ob2.remove_all();
+    ob2.show();
     return 0;
 };
 
