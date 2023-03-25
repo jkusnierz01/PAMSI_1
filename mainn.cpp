a511b3d27edd4c9cffb173bcfedb0747f3e78ba5
jkusnierz01 <jedrek33@onet.pl>
2023-03-25 15:02:40 +0100

    przed testami jednostkowymi

--- mainn.cpp
+++ mainn.cpp
@@ -1,5 +1,3 @@
-#include "array_stack.hh"
-#include "list_stack.hh"
 #include "queue.hh"
 #include <cstdlib>
 #include <unistd.h>
@@ -9,10 +7,10 @@ int main()
 {
     bool x=1;
     int size;
-    int tab[size];
     queue<int> kolejka;
     cout << "Podaj ilość elementw w kolejce: ";
     cin >> size;
+    int tab[size];
     for (int i = 0; i < size+1; i++)
     {
         tab[i] = rand();
@@ -21,13 +19,27 @@ int main()
     {
         for (int x = 0; x < size+1; x++)
         {
-            usleep(rand()%2*1000000);
+            float sleep = float(rand()%100*1000);
+            usleep(sleep);
             kolejka.push(tab[x]);
-            cout << "Rozmiar struktury" << sizeof(kolejka);
+            cout << "Rozmiar struktury: " << sizeof(kolejka) << endl;;
+            cout << "Ostatni element w kolejce: " << kolejka.TailPointer->Value << endl;
         }
-        
+        x=0;
     }
+    return 0;
 };
+
+
+/*
+Czy aktualny rozmiar skuktury to po prostu wartość zwaraca przez funkcje sizeof()?
+W jaki sposob program rowonoczesnie ma wrzucac elementy i wypisywac na ekranie wielkosc stuktury?
+"...pobranie najstarszej informacji przechowywanej w strukturze" - wypisać po prostu na ekranie?
+
+
+
+
+*/
 
 
 
